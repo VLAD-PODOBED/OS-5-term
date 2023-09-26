a1184174d410e1246293743a4cea564853fe89ff
Vlad <mama2003271@gmail.com>
2023-09-26 15:49:25 +0300

    add

---
+++ LABA4/os4_1/os4_1/os4_1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <process.h>
+
+int main()
+{
+    DWORD pid = GetCurrentProcessId();
+    DWORD tid = GetCurrentThreadId();
+
+    for (int i = 0; i < 10000; i++) {
+        Sleep(1000);
+        std::cout << "PID = " << pid << "\tTID = " << tid << "\n";
+    }
+}
